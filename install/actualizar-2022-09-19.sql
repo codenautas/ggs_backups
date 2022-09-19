1857db20921cedc73a4938132816d30e1afc4b37
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2022-09-19 20:22:29 -0300

    aplicadas modificaciones de Raquel para sql de matching de grillas

--- install/actualizar-2022-09-19.sql
+++ install/actualizar-2022-09-19.sql
@@ -1,22 +1,21 @@
 GRANT SELECT ON TABLE base.tareas_tem TO ggs_backups_admin;
 GRANT SELECT ON TABLE base.tem TO ggs_backups_admin;
 
-/* sql correspondiente a la grilla de match 
-camposa a gregar en la defincion : rea, norea,verif_encurecu ( o verif_campo)
+-- sql correspondiente a la grilla de match 
+-- campos a a gregar en la defincion : rea, norea,verif_encurecu ( o verif_campo)
 select th.*,
-                p.nombre, p.edad, b.age ageb, p.sexo, b.dem01 dem01b, b.asex asexb, p.nacms, b.dobm dobmb, b.doby dobyb,
-                t.rea, t.norea, 
-                tt.verif_campo,
-                b.*
-                from backups b 
-                join base.tem_hogar th on (b.respid = th.idblaise)
-                join base.tem t on t.operativo=th.operativo and t.enc=th.enc
-                left join base.personas p on (p.operativo = th.operativo AND p.vivienda = th.enc AND p.hogar = th.hogar AND th.idblaise = p.id_blaise::integer)
-                left join lateral (
-                    select max(tt.verificado) verif_campo from base.tareas_tem tt where tt.operativo = th.operativo AND tt.enc=th.enc and tarea in ('encu','recu') and verificado ='1'
-                ) as tt on true
-                left join backups otrob on b.respid=otrob.respid and otrob.lote <>b.lote and otrob.verificado_procesamiento
-                where b.verificado_procesamiento 
-                or (b.lote = (select max(lote) from lotes) and
-                    otrob.respid is null);
-*/                    
+    p.nombre, p.edad, b.age ageb, p.sexo, b.dem01 dem01b, b.asex asexb, p.nacms, b.dobm dobmb, b.doby dobyb,
+    t.rea, t.norea, 
+    tt.verif_campo,
+    b.*
+    from backups b 
+    join base.tem_hogar th on (b.respid = th.idblaise)
+    join base.tem t on t.operativo=th.operativo and t.enc=th.enc
+    left join base.personas p on (p.operativo = th.operativo AND p.vivienda = th.enc AND p.hogar = th.hogar AND th.idblaise = p.id_blaise::integer)
+    left join lateral (
+        select max(tt.verificado) verif_campo from base.tareas_tem tt where tt.operativo = th.operativo AND tt.enc=th.enc and tarea in ('encu','recu') and verificado ='1'
+    ) as tt on true
+    left join backups otrob on b.respid=otrob.respid and otrob.lote <>b.lote and otrob.verificado_procesamiento
+    where b.verificado_procesamiento 
+    or (b.lote = (select max(lote) from lotes) and
+        otrob.respid is null);
--- src/server/grilla_match_id.ts
+++ src/server/grilla_match_id.ts
@@ -23,9 +23,11 @@ export function grilla_match_id(): TableDefinition {
          {name:'nacms'     ,  typeName: 'text'   ,  editable: false },
          {name:'dobmb'     ,  typeName: 'text'   ,  label:'mes backup', editable: false },
          {name:'dobyb'     ,  typeName: 'text'   ,  label:'año backup', editable: false },
-
-         //agregar resultado de la tem?
-         //agregar como detalle el registro de personas y/hogar/tem
+        //campos base.tem
+        {name:'rea'      ,  typeName: 'integer',  editable: false },
+        {name:'norea'      ,  typeName: 'integer',  editable: false },
+        //campos base.tareas_tem
+        {name:'verif_campo'      ,  typeName: 'text',  editable: false },
     ]
     //@ts-ignore
     def.fields=[...tem_hogar_fields, ...def.fields]
@@ -34,11 +36,16 @@ export function grilla_match_id(): TableDefinition {
             isTable:false,
             from:`(select th.*,
                 p.nombre, p.edad, b.age ageb, p.sexo, b.dem01 dem01b, b.asex asexb, p.nacms, b.dobm dobmb, b.doby dobyb,
-
+                t.rea, t.norea, 
+                tt.verif_campo,
                 b.*
                 from backups b 
                 join base.tem_hogar th on (b.respid = th.idblaise)
+                join base.tem t on t.operativo=th.operativo and t.enc=th.enc
                 left join base.personas p on (p.operativo = th.operativo AND p.vivienda = th.enc AND p.hogar = th.hogar AND th.idblaise = p.id_blaise::integer)
+                left join lateral (
+                    select max(tt.verificado) verif_campo from base.tareas_tem tt where tt.operativo = th.operativo AND tt.enc=th.enc and tarea in ('encu','recu') and verificado ='1'
+                ) as tt on true
                 left join backups otrob on b.respid=otrob.respid and otrob.lote <>b.lote and otrob.verificado_procesamiento
                 where b.verificado_procesamiento 
                 or (b.lote = (select max(lote) from lotes)) and
