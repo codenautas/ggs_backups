e786636c549d35073b899b5af34eefc445c46708
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2022-09-27 15:57:49 -0300

    grillas cambios Raquel para mejorar condiciones verfi_campo

--- src/server/grilla_match_id.ts
+++ src/server/grilla_match_id.ts
@@ -57,21 +57,25 @@ export function grilla_match_id(): TableDefinition {
         {
             isTable:false,
             from:`(select th.*,
-                p.nombre, p.edad, b.age ageb, p.sexo, b.dem01 dem01b, b.asex asexb, p.nacms, b.dobm dobmb, b.doby dobyb,
-                t.rea, t.norea, 
-                tt.verif_campo,
-                b.*
-                from backups b 
-                join base.tem_hogar th on (b.respid = th.idblaise)
-                join base.tem t on t.operativo=th.operativo and t.enc=th.enc
-                left join base.personas p on (p.operativo = th.operativo AND p.vivienda = th.enc AND p.hogar = th.hogar AND th.idblaise = p.id_blaise::integer)
-                left join lateral (
-                    select max(tt.verificado) verif_campo from base.tareas_tem tt where tt.operativo = th.operativo AND tt.enc=th.enc and tarea in ('encu','recu') and verificado ='1'
-                ) as tt on true
-                left join backups otrob on b.respid=otrob.respid and otrob.lote <>b.lote and otrob.verificado_procesamiento
-                where b.verificado_procesamiento 
-                or (b.lote = (select max(lote) from lotes)) and
-                    otrob.respid is null)`,
+                    p.nombre, p.edad, b.age ageb, p.sexo, b.dem01 dem01b, b.asex asexb, p.nacms, b.dobm dobmb, b.doby dobyb,
+                    t.rea, t.norea, 
+                    tt.verif_campo,
+                    b.*
+                    from backups b 
+                    join base.tem_hogar th on (b.respid = th.idblaise)
+                    join base.tem t on t.operativo=th.operativo and t.enc=th.enc
+                    left join base.personas p on (p.operativo = th.operativo AND p.vivienda = th.enc AND p.hogar = th.hogar AND th.idblaise = p.id_blaise::integer)
+                    left join lateral (
+                        select string_agg(substr(tt.tarea,1,1),'-'order by tt.tarea)t,count(*)nt, count(verificado) ntv,
+                            case when count(*) = count(verificado) then max(tt.verificado)else null end verif_campo 
+                            from base.tareas_tem tt 
+                            where tt.operativo = th.operativo AND tt.enc=th.enc 
+                                    and tarea in ('encu','recu') and asignado is not null and operacion is not null
+                    ) as tt on true
+                    left join backups otrob on b.respid=otrob.respid and otrob.lote <>b.lote and otrob.verificado_procesamiento
+                    where b.verificado_procesamiento 
+                        or (b.lote = (select max(lote) from lotes)) and
+                        otrob.respid is null)`,
             insertIfNotUpdate:false
         })
   return def;
