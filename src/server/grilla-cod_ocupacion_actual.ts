f6f13ba86c10fdd3d72e2892b4acc5732da41719
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2022-09-27 16:11:28 -0300

    reordenamiento campos codificación

--- src/server/grilla-cod_ocupacion_actual.ts
+++ src/server/grilla-cod_ocupacion_actual.ts
@@ -5,7 +5,7 @@ import { TableDefinition } from "./types-ggs_backups";
 
 export function cod_ocupacion_actual(): TableDefinition {
     let def = cod_base(
-        ['dem06', 'dem07', 'wrk02', 'wrk04_3601', 'wrk04_3602', 'wrk06', 'wrk07', 'wrk09', 'wrk10', 'wrk11', 'wrk12', 'wrk14', 'wrk17', 'wrk18', 'wrk22', 'wrk23'], 
+        ['dem06', 'dem07', 'wrk17', 'wrk04_3601', 'wrk04_3602', 'wrk02', 'wrk06', 'wrk07', 'wrk09', 'wrk10', 'wrk11', 'wrk12', 'wrk14', 'wrk18', 'wrk22', 'wrk23'], 
         ['cno_ocup_actual', 'ciuo_ocup_actual']
     );
     def.title += ' ocupación actual'
